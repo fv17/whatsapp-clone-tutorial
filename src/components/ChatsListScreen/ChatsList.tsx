aa921497683287abf51c0abe8c99e85a5d9dc9a2
fv17 <forest.valley0417@gmail.com>
2020-01-01 15:56:37 +0900

    add: コンポーネント化, Fake dataの準備

---
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { chats } from '../../db';
+
+const ChatsList: React.FC = () => (
+  <div>
+    <ul>
+      {chats.map(chat => (
+        <li key={chat.id}>
+          <img src={chat.picture} alt="Profile" />
+          <div>{chat.name}</div>
+          {chat.lastMessage && (
+            <React.Fragment>
+              <div>{chat.lastMessage.content}</div>
+              <div>{chat.lastMessage.createdAt}</div>
+            </React.Fragment>
+          )}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default ChatsList;
