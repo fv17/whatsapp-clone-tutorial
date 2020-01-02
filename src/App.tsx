8b7652731b45c9938d70938d05767a757881a0ef
fv17 <forest.valley0417@gmail.com>
2020-01-02 17:44:28 +0900

    add: Add basic ChatRoomScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+  RouteComponentProps,
+} from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <ChatsListScreen />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/chats" component={ChatsListScreen} />
+        <Route
+          exact
+          path="/chats/:chatId"
+          component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} />
+          )}
+        />
+      </Switch>
+      <Route exact path="/" render={redirectToChats} />
+    </BrowserRouter>
   );
 };
+
+const redirectToChats = () => <Redirect to="/chats" />;
 
 export default App;
